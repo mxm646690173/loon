b9346ad543e7e2583467b9660b74e9c1659f334c
Illusion <57613270+mxm646690173@users.noreply.github.com>
2025-01-10 13:19:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,11 @@
 // 定义一个函数来处理请求
-function handleRequest(request) {
-    Notify_Demo(request)
-}
-
-// 监听请求
-$loon.onRequest(handleRequest);
-function Notify_Demo(request)
+function Notify_Demo()
 {
     var Title,SubTitle,Content;
     TItle = "我是主标题";
     SubTitle  = "我是子标题";
-    Content = request;
+    Content = $request;
     $notification.post(Title,SubTitle,Content);
 }
-//Call Function
 
+Notify_Demo
