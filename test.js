032f024dc75d62c5981a7d41c750b74f9c92d02f
Illusion <57613270+mxm646690173@users.noreply.github.com>
2025-01-10 13:09:34 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 // 定义一个函数来处理请求
-function handleRequest(request) {
-    console.log(request)
-}
+//function handleRequest(request) {
+//    console.log(request)
+//}
 
 // 监听请求
-$loon.onRequest(handleRequest);
+//$loon.onRequest(handleRequest);
+console.log('正则匹配成功')
